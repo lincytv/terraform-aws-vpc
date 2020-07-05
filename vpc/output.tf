f10bc0b4412abcd82f4d235e286922db2f8b4abc
lincytv <lincytv@gmail.com>
2020-07-05 14:23:13 +0530

    vpc

---
+++ vpc/output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.elk_internal.id
+}
+
+output "public_subnet_01" {
+  value = aws_subnet.public_subnet_01.id
+}
+
+output "private_subnet_01" {
+  value = aws_subnet.private_subnet_01.id
+}
